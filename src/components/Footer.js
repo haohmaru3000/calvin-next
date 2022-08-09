d14e52e8d82ae6174fe43314d961f62ab7fd6c2a
tuanit168 <tuanit168@gmail.com>
2022-08-09 11:00:43 +0700

    Session 9: install and config @tailwindcss/forms

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 function Footer() {
   return (
     <footer className="py-4 flex justify-center font-primary items-center">
-      Jewelry Shop{" "}
+      CTK Shop{" "}
       <FontAwesomeIcon icon={faHeart} className="w-5 text-red-600 mx-1" /> of{" "}
       <a
         href="https://twitter.com/deepwhitman"
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,35 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      height: (theme) => ({
+        112: "28rem",
+        120: "30rem",
+      }),
+      minHeight: (theme) => ({
+        80: "20rem",
+      }),
+      colors: {
+        palette: {
+          lighter: "#F5F3FF",
+          light: "#DDD6FE",
+          primary: "#5B21B6",
+          dark: "#4C1D95",
+        },
+      },
+      fontFamily: {
+        primary: ['"Josefin Sans"'],
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "class",
+    }),
+  ],
 };
